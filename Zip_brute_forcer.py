ef5f5ad19a43672c90db4d3d163881048332c1b2
Matthew Franks <42283335+mafranks@users.noreply.github.com>
2019-08-13 10:48:57 -0400

    Create Zip_brute_forcer.py

---
+++ Zip_brute_forcer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+#usage:
+#python3 Zip_brute_forcer.py 'filename.zip' 'passwordList.txt'
+
+from zipfile import ZipFile
+import os
+import sys
+
+directory = os.path.abspath('.')
+filename = sys.argv[1]
+passwordFile = sys.argv[2]
+count = 0
+fileLength = len(open(f'{directory}/{passwordFile}', errors='ignore').readlines())
+with ZipFile(f'{directory}/{filename}') as zip:
+    with open(f'{directory}/{passwordFile}', errors='ignore') as f:
+        for passwrd in f.readlines():
+            passwrd = passwrd.strip()
+            try:
+                zip.extractall(path=directory, pwd=passwrd.encode()) 
+                exit(f'Success! The password is "{passwrd}"')
+            except Exception:
+                if count % 10000 == 0:
+                    print(f'Processing: {count}/{fileLength}')
+                count += 1
+                
+exit('Password not found in the list.')
